ca59e1fb1b8718d136f691f122eda81e0740fabc
Hashantha Perera <7hashd@gmail.com>
2019-03-17 14:52:35 +0530

    intent

--- app/src/main/java/com/example/flag/MainActivity.java
+++ app/src/main/java/com/example/flag/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.flag;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button guesstheCountry = (Button) findViewById(R.id.button_guesstheCountry);
+        Button guessHints = (Button) findViewById(R.id.button_guessHints);
+        Button guessTheFlag = (Button) findViewById(R.id.button_guessTheFlag);
+        Button advancedLevel = (Button) findViewById(R.id.button_advancedLevel);
+
+
+        guesstheCountry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),GuessTheCountry.class);
+                startActivity(intent);
+            }
+        });
     }
 }
