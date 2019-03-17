a933f941b79221ef88d85854c7cf137f610e8bd6
Hashantha Perera <7hashd@gmail.com>
2019-03-17 12:39:33 +0530

    ...

---
+++ app/src/main/java/com/example/flag/GuessTheCountry.java
@@ -0,0 +1,13 @@
+package com.example.flag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuessTheCountry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_the_country);
+    }
+}
