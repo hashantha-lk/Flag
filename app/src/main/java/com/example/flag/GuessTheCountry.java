3435e56d7075228f2ee746065c8fb4c1753ec791
Hashantha Perera <7hashd@gmail.com>
2019-03-20 02:23:43 +0530

    guess the country done !

--- app/src/main/java/com/example/flag/GuessTheCountry.java
+++ app/src/main/java/com/example/flag/GuessTheCountry.java
@@ -1,5 +1,7 @@
 package com.example.flag;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -22,19 +24,24 @@ public class GuessTheCountry extends AppCompatActivity {
 
         //Creating usable objects.
         FlagImage flag = new FlagImage();
-        final CountryName country = new CountryName();
         ImageView image_country = (ImageView) findViewById(R.id.image_country);
+        final TextView textView_status = (TextView) findViewById(R.id.textView_status);
+        final TextView textView_correct_country = (TextView) findViewById(R.id.textView_correct_country);
+        final CountryName country = new CountryName();
         final Spinner spinner_country = (Spinner) findViewById(R.id.spinner_country);
         final Button submit = (Button) findViewById(R.id.button_submit);
+
 
         //Random method used to randomize the flag image.
         Random randomFlagGenerator = new Random();
         final int number = randomFlagGenerator.nextInt(256);
         image_country.setImageResource(flag.flag_array[number]);
 
+
         //ArrayAdapter used to add items to the spinner.
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(GuessTheCountry.this, android.R.layout.simple_spinner_dropdown_item, country.country_array);
         spinner_country.setAdapter(adapter);
+
 
         //On item selected listener to get user's selection.
         spinner_country.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -48,12 +55,37 @@ public class GuessTheCountry extends AppCompatActivity {
                     public void onClick(View v) {
                         int country_index = spinner_country.getSelectedItemPosition();
                         int flag_index = number;
+                        for (int i = 0; i <= 1000; i++) {
+                            submit.setText("Submit");
+                            if (country_index == flag_index) {
+                                textView_status.setText("Correct !");
+                                textView_status.setTextColor(Color.GREEN);
+                                submit.setText("Next");
 
-                        if (country_index == flag_index) {
-                            Toast.makeText(GuessTheCountry.this, "Correct !", Toast.LENGTH_SHORT).show();
+                                submit.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent intent = new Intent(getApplicationContext(), GuessTheCountry.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                });
+                            } else {
+                                textView_status.setText("Wrong !");
+                                textView_status.setTextColor(Color.RED);
+                                textView_correct_country.setText(country.country_array[number]);
+                                textView_correct_country.setTextColor(Color.BLUE);
+                                submit.setText("Next");
 
-                        } else {
-                            Toast.makeText(GuessTheCountry.this, "Wrong !\n\n" + country.country_array[number], Toast.LENGTH_LONG).show();
+                                submit.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent intent = new Intent(getApplicationContext(), GuessTheCountry.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                });
+                            }
                         }
                     }
                 });
--- app/src/main/java/com/example/flag/MainActivity.java
+++ app/src/main/java/com/example/flag/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         guesstheCountry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),GuessTheCountry.class);
+                Intent intent = new Intent(getApplicationContext(), GuessTheCountry.class);
                 startActivity(intent);
             }
         });
