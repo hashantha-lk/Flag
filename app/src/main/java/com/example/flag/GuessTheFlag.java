682f31e5ff110fd5d2ce3409a74ae9b105a85703
Hashantha Perera <7hashd@gmail.com>
2019-03-20 12:23:21 +0530

    guess the flag

--- app/src/main/java/com/example/flag/GuessTheFlag.java
+++ app/src/main/java/com/example/flag/GuessTheFlag.java
@@ -1,7 +1,10 @@
 package com.example.flag;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -17,22 +20,23 @@ public class GuessTheFlag extends AppCompatActivity {
 
         //Objects in use.
         FlagImage flag = new FlagImage();
-        CountryName country = new CountryName();
+        final CountryName country = new CountryName();
         ImageButton flag1 = (ImageButton) findViewById(R.id.imageButton_flag1);
         ImageButton flag2 = (ImageButton) findViewById(R.id.imageButton_flag2);
         ImageButton flag3 = (ImageButton) findViewById(R.id.imageButton_flag3);
         TextView flag_name = (TextView) findViewById(R.id.textView_flag_name);
-        TextView status = (TextView) findViewById(R.id.textView_status1);
-        TextView correct_country = (TextView) findViewById(R.id.textView_correct_country1);
-        Button btn = (Button) findViewById(R.id.button_submit1);
+        final TextView status = (TextView) findViewById(R.id.textView_status1);
+        final TextView correct_country = (TextView) findViewById(R.id.textView_correct_country1);
+        final Button next = (Button) findViewById(R.id.button_next);
 
         //Random method used to randomize the flag button image.
         Random randomFlag = new Random();
 
-        int num1 = randomFlag.nextInt(256);
-        int num2 = randomFlag.nextInt(256);
-        int num3 = randomFlag.nextInt(256);
+        final int num1 = randomFlag.nextInt(256);
+        final int num2 = randomFlag.nextInt(256);
+        final int num3 = randomFlag.nextInt(256);
 
+        //Validatint not to repeat the same flag twice or thrice.
         if (num1 != num2 && num1 == num3 && num2 == num3 && num3 == num1) {
 
         } else {
@@ -40,10 +44,124 @@ public class GuessTheFlag extends AppCompatActivity {
             flag2.setImageResource(flag.flag_array[num2]);
             flag3.setImageResource(flag.flag_array[num3]);
         }
+
+        //Randomizing the country names in the image buttons to display in top text view flag_name.
         int[] countryIndex = {num1, num2, num3};
         Random randomCountry = new Random();
         int flagIndex = randomCountry.nextInt(3);
         flag_name.setText(country.country_array[3]);
 
+        //Selection part.
+        if (num1 == flagIndex) {
+            flag1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    status.setText("Correct !");
+                    status.setTextColor(Color.GREEN);
+
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getApplicationContext(), GuessTheFlag.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+            });
+        } else {
+            flag1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    status.setText("Wrong !");
+                    status.setTextColor(Color.RED);
+                    correct_country.setText(country.country_array[num1]);
+
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getApplicationContext(), GuessTheFlag.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+            });
+
+        }
+        if (num2 == flagIndex) {
+            flag2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    status.setText("Correct !");
+                    status.setTextColor(Color.GREEN);
+
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getApplicationContext(), GuessTheFlag.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+            });
+        } else {
+            flag2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    status.setText("Wrong !");
+                    status.setTextColor(Color.RED);
+                    correct_country.setText(country.country_array[num2]);
+
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getApplicationContext(), GuessTheFlag.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+            });
+
+        }
+        if (num3 == flagIndex) {
+            flag3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    status.setText("Correct !");
+                    status.setTextColor(Color.GREEN);
+
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getApplicationContext(), GuessTheFlag.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+            });
+        } else {
+            flag3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    status.setText("Wrong !");
+                    status.setTextColor(Color.RED);
+                    correct_country.setText(country.country_array[num3]);
+
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getApplicationContext(), GuessTheFlag.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+            });
+
+        }
     }
 }
